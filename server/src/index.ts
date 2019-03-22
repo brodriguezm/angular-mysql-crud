dd61ee3d01fd7bb254cdf25ee00286dbad1587e7
Bryan Rodriguez <brodriguez@grupo-link.com>
2019-03-22 18:03:49 -0500

    config server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,30 @@
-console.log('hola ts')
-console.log('hola ts2')
-console.log('hola ts3')
+import express, { Application } from "express";
+ 
+
+class Server{
+
+    public app:  Application;
+    constructor(){
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+
+    config(): void {
+        this.app.set('port', process.env.PORT || 3000);
+    }
+
+    routes(): void {
+
+    }
+
+    start(): void {
+        this.app.listen(this.app.get('port'), () => {
+            console.log('Server listen on port ' + this.app.get('port'));
+        });
+    }
+
+}
+
+const server = new Server();
+server.start();
