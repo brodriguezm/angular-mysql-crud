0aac35fce2fffb1b168f64187700156e34b382fc
Bryan Rodriguez <bsteven1993@gmail.com>
2019-04-03 12:20:50 -0500

    vista formulario game

--- client/src/app/components/game-form/game-form.component.ts
+++ client/src/app/components/game-form/game-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-game-form',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-form.component.css']
 })
 export class GameFormComponent implements OnInit {
+
+  @HostBinding('class') classes = 'row';
 
   constructor() { }
 
